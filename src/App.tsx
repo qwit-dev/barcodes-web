1f200c8f251974832d7973d480065bd740a490dd
sms mester <s.mate3d@gmail.com>
2024-09-23 09:32:59 +0200

    first shit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import qr1 from '/images/qr/qr1.png?url';
+function App() {
+  return (
+      <div className="flex flex-row items-center justify-center h-max flex-wrap gap-[30px]">
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+          <div
+              className="bg-neutral-800 flex flex-col items-center justify-center rounded-2xl py-[20px] px-[16px] cursor-pointer">
+              <img src={qr1} alt="qr1" className="rounded-xl"/>
+              <p className="text-xl font-bold text-white">
+                  Pepsi Max - 1L, PET
+              </p>
+          </div>
+      </div>
+  )
+}
+
+export default App
